2ea248f36351696a307d266e91cdacce3ffe2c01
sven <sven@so36.net>
2021-12-17 15:11:55 +0100

    code(init) add sub command for init docs

--- cmd/initDocs.go
+++ cmd/initDocs.go
@@ -21,31 +21,31 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// addCmd represents the add command
-var addCmd = &cobra.Command{
-	Use:   "add",
-	Short: "Add a part of a component",
-	Long: `Add a part of a component. For example:
-- Dockerfile
-- README
-
-Use this if you want to extent your component.`,
+// initDocsCmd represents the initDocs command
+var initDocsCmd = &cobra.Command{
+	Use:   "docs",
+	Short: "Setup linting for documentation",
+	Long: `Enables the following linting tools:
+- alex for prose
+- markdown-lint for Markdown
+- markdown-link-checker
+`,
 	PreRun: toggleDebug, // This is for logging.
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Error: must also specify a resource")
+		fmt.Println("initDocs called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(addCmd)
+	initCmd.AddCommand(initDocsCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// initDocsCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// initDocsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
