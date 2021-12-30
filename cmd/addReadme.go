31aa053776c037be550fdd878321bb342bfaed03
sven <sven@so36.net>
2021-12-30 16:26:50 +0100

    chore(goreleaser) update config

--- cmd/addReadme.go
+++ cmd/addReadme.go
@@ -16,20 +16,20 @@ limitations under the License.
 package cmd
 
 import (
-	"errors"
+	//"errors"
 	"fmt"
 	"os"
-	"text/template"
+	//"text/template"
 
 	"github.com/fatih/color"
-	"github.com/manifoldco/promptui"
+	//"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
 
-type Todo struct {
-	Name        string
-	Description string
-}
+//type Todo struct {
+//	Name        string
+//	Description string
+//}
 
 // addReadmeCmd represents the addReadme command
 var addReadmeCmd = &cobra.Command{
@@ -38,47 +38,48 @@ var addReadmeCmd = &cobra.Command{
 	Long:   `Add a README to a project.`,
 	PreRun: toggleDebug, // This is for logging.
 	Run: func(cmd *cobra.Command, args []string) {
-		color.Green("Processing")
 		addReadmeFile()
 	},
 }
 
 func addReadmeFile() {
-	validate := func(input string) error {
-		if len(input) < 3 {
-			return errors.New("project name must have more than 3 characters")
-		}
-		return nil
-	}
+	//validate := func(input string) error {
+	//	if len(input) < 3 {
+	//		return errors.New("project name must have more than 3 characters")
+	//	}
+	//	return nil
+	//}
 
-	prompt := promptui.Prompt{
-		Label:    "Name",
-		Validate: validate,
-		Default:  "My-Cool-Project",
-	}
+	//prompt := promptui.Prompt{
+	//	Label:    "Name of the Project",
+	//	Validate: validate,
+		//Default:  "My-Cool-Project",
+	//}
 
-	result, err := prompt.Run()
-	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
-		return
-	}
+	//result, err := prompt.Run()
+	//if err != nil {
+	//	fmt.Printf("Prompt failed %v\n", err)
+	//	return
+	//}
 
 	vars := make(map[string]interface{})
-	vars["Name"] = result
+	//vars["Name"] = result
+	vars["Name"] = "Dev"
 
 	// Check if file, exists, if yes fail with error message
 	if _, err := os.Stat("README.gen.md"); err == nil {
 		fmt.Printf("File already exists\nRun 'cotterpin add readme -f' to overwrite it")
 	} else {
 		// Parse the template
-		tmpl, _ := template.ParseFiles("templates/readme.tmpl")
+		//tmpl, _ := template.ParseFiles("templates/readme.tmpl")
 
 		// Create a new file
-		file, _ := os.Create("README.gen.md")
-		defer file.Close()
+		color.Green("Creating README")
+		//file, _ := os.Create("README.gen.md")
+		//defer file.Close()
 
 		// Apply the template to the vars map and write the result to file.
-		tmpl.Execute(file, vars)
+		//tmpl.Execute(file, vars)
 	}
 }
 
@@ -94,5 +95,5 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// addReadmeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	addReadmeCmd.Flags().StringP("name", "n", "", "Name of the project, for example My-Cool-Project")
+	//addReadmeCmd.Flags().StringP("name", "n", "", "Name of the project, for example My-Cool-Project")
 }
